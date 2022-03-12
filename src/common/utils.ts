a4366bd939e82f3ac47185be068de4842851763f
Andrei <vitobravis@gmail.com>
2022-03-12 16:40:44 +0700

    update common mixins & functions

--- src/common/utils.ts
+++ src/common/utils.ts
@@ -14,3 +14,32 @@ export const findComponents = <T extends Element = Element>(
         name,
         node,
     }));
+
+export const emit = <T = any>(name: string, data?: T, element: Document | HTMLElement = document, shouldBubble = false) => {
+    let evt;
+    if (typeof CustomEvent === 'function') {
+        evt = new CustomEvent(name, {
+            detail: data,
+            bubbles: shouldBubble,
+        });
+    } else {
+        evt = document.createEvent('CustomEvent');
+        evt.initCustomEvent(name, shouldBubble, false, data);
+    }
+    element.dispatchEvent(evt);
+};
+
+export const listen = (name: string, handler: (e?: CustomEvent) => void, element: Document | Element = document) => {
+    element.addEventListener(name, handler);
+};
+
+export const unlisten = (name: string, handler: (e?: CustomEvent) => void, element: Document | Element = document) => {
+    element.removeEventListener(name, handler);
+};
+
+export const isIe = () => {
+    const agent = window.navigator.userAgent;
+    const msie = agent.indexOf('MSIE ');
+
+    return msie > -1 || !!navigator.userAgent.match(/Trident.*rv:11\./);
+};
