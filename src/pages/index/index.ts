1172a3648b90e691048c2b14cbbc56f62adcb02d
Timofey Chunarev <timofei.chunarev@gmail.com>
2022-03-21 17:40:11 +0700

    init swiper and slider into index.ts

--- src/pages/index/index.ts
+++ src/pages/index/index.ts
@@ -4,15 +4,29 @@ import { findComponent } from '@common/utils';
 // import Counter from '@components/counter/counter';
 import CounterOfActiveSlide from '@components/counterOfActiveSlide/counterOfActiveSlide';
 import ModalWindow from '@components/modalWindow/modalWindow';
+import Slider from '@components/slider/slider';
+import { Swiper, SwiperOptions } from 'swiper';
 
 export default {
     namespace: 'index',
     beforeEnter(data: IViewData) {},
     afterEnter(data: IViewData) {
         // const counter = new Counter(findComponent('counter'));
-        function buttonAction() {
-            console.log('Действие выполнено!!!');
-        }
+        // function buttonAction() {
+        //     console.log('Действие выполнено!!!');
+        // }
+
+        const swiperParams: SwiperOptions = {
+            slidesPerView: 1,
+            spaceBetween: 50,
+            slideActiveClass: 'swiper-active-slide',
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+        };
+
+        const swiper = new Swiper('.swiper', swiperParams);
 
         const modalWindow = new ModalWindow(
             findComponent('modalWindow', 'modal-window')
@@ -21,7 +35,7 @@ export default {
             findComponent('counterOfActiveSlide', 'counter-of-active-slide'),
             modalWindow
         );
-        console.log(modalWindow);
+        const slider = new Slider(findComponent('slider'), swiper);
         // counterOfActiveSlide.nextCount();
         // console.log(counterOfActiveSlide, 'Это мой counter!');
     },
