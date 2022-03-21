2455cf5f114544f2a592bd82bf88c4c2989404fc
Timofey Chunarev <timofei.chunarev@gmail.com>
2022-03-21 14:45:36 +0700

    create slider component

---
+++ src/components/slider/slider.ts
@@ -0,0 +1,11 @@
+import RootComponent from '@common/rootComponent';
+import { IRootComponent } from '@common/types';
+import Swiper from 'swiper';
+
+class Slider extends RootComponent {
+    constructor(props: IRootComponent) {
+        super(props);
+    }
+}
+
+export default Slider;
