c23600144c20075a0b78a6f68dce8c1ef419558f
Timofey Chunarev <timofei.chunarev@gmail.com>
2022-03-26 10:01:36 +0700

    create swiperSlide component

---
+++ src/components/swiperSlide/swiperSlide.ts
@@ -0,0 +1,10 @@
+import RootComponent from '@common/rootComponent';
+import { IRootComponent } from '@common/types';
+
+class SwiperSlide extends RootComponent {
+    constructor(props: IRootComponent) {
+        super(props);
+    }
+}
+
+export default SwiperSlide;
